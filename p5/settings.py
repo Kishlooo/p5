0e8a33cab470c4c0ed0c442604fea12f6a19cb4c
kishlooo <kishanbamrotiya25@gmail.com>
2020-08-06 22:23:34 +0530

    third commit

--- p5/settings.py
+++ p5/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR=os.path.join(BASE_DIR,"templates")
+STATIC_DIR_MYAPP=os.path.join(os.path.join(BASE_DIR,"myapp"),"static")
+STATIC_DIR=os.path.join(BASE_DIR,"static")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -117,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT=STATIC_DIR#parent static directory
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    STATIC_DIR_MYAPP,
+] 
