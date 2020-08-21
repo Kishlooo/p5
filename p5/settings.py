fc12d0043227da3cade1c6c3797e776f6f40ca83
kishlooo <kishanbamrotiya25@gmail.com>
2020-08-21 12:33:43 +0530

    tenth commit

--- p5/settings.py
+++ p5/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
